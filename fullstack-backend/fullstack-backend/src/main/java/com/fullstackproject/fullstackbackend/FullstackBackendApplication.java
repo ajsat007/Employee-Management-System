71db2de2502d4986fabd874b265efd9457bb667c
Ajinkya Satkar <87180291+ajsat007@users.noreply.github.com>
2023-04-29 16:54:43 +0530

    Add files via upload

---
+++ fullstack-backend/fullstack-backend/src/main/java/com/fullstackproject/fullstackbackend/FullstackBackendApplication.java
@@ -0,0 +1,14 @@
+package com.fullstackproject.fullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class FullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackBackendApplication.class, args);
+	}
+
+}
